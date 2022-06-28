c36b7b82cbde608208eec59426e51b4be4cb8bfc
joaodmonteiro <joaodmonteiro10@gmail.com>
2022-06-28 13:44:26 +0100

    individual movie pages

--- pages/movies/[id].js
+++ pages/movies/[id].js
@@ -1,5 +1,26 @@
+import Link from "next/link";
+import styles from "../../styles/movie.module.scss";
+
 export default function Movie({ movie }) {
-  return <div>{movie.title}</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.banner_container}>
+        <img src={movie.movie_banner} alt="" />
+      </div>
+      <div className={styles.info}>
+        <h1 className={styles.title}>{movie.title}</h1>
+        <h2 className={styles.director}>Director: {movie.director}</h2>
+        <h2 className={styles.release_date}>
+          Release Date: {movie.release_date}
+        </h2>
+        <Link href="/">
+          <div className={styles.back_button}>
+            <div>BACK</div>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export async function getServerSideProps(context) {
